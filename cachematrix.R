58533915c40d173503552a8dd62eefe2227e548f
ronrest <roger.mirble@gmail.com>
2015-02-22 23:12:45 +1100

    Add two lines of comments to test if I can get this to be pushed to github

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,19 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+# First edit, just to see if committing to Git will work. 
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
+# Another comment in the middle of the text, just to see how Git will deal with it. 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+
